5fa30d015349af91dd7909059725553d9a3c33a8
julkarlase <julkaryalase@gmail.com>
2022-09-18 12:35:20 +0700

    add controllers contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_controllers
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>"
+        echo "nama saya julkaria harapan jaya lase
+            saya tinggal di daerah jakarta
+            olah raga yang saya sukai adalah
+            bulutangkis";
+            
+    }
+}
